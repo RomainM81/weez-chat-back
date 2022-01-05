6432cea669da7503df65b7b79070b042f8c9b662
RomainM81 <dcromain112@gmail.com>
2022-01-05 11:17:32 +0000

    add table!

--- routes/channels.js
+++ routes/channels.js
@@ -5,25 +5,38 @@ const router = express.Router()
 router.get('/', (req, res) => {
   const usersID = [req.query.id1, req.query.id2]
   mysql.query('SELECT * FROM channels WHERE id_user_1 = ? AND id_user_2 = ?',
-  usersID), (err, result) => {
+  usersID, (err, result) => {
     if(err) {
       res.status(500).send('No channel found ' + err)
     } else {
-      res.status(200).json(result)
+      if(Object.keys(result).length === 0) {
+        console.log('channel non present')
+        mysql.query('INSERT INTO channels (id_user_1, id_user_2) VALUES (?, ?)',
+    usersID, (err, result2) => {
+    if(err) {
+      res.status(500).send('Error' + err)
+    } else {
+      res.status(200).json(result2)
     }
-  }
+      })
+      } else {
+        res.status(200).json(result)
+      }
+        } 
+    
+  })
 })
 
 router.post('/', (req, res) => {
   const usersID = [req.query.id1, req.query.id2]
   mysql.query('INSERT INTO channels (id_user_1, id_user_2) VALUES (?, ?)',
-  usersID), (err, result) => {
+  usersID, (err, result) => {
     if(err) {
       res.status(500).send('No channel found ' + err)
     } else {
       res.status(200).json(result)
     }
-  }
+  })
 })
 
 module.exports = router
